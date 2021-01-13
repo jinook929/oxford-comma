9f820445a3515e009d4626affa1355d39318cd5d
jinook929 <jinook929@gmail.com>
2021-01-13 14:32:41 -0500

    commit tree df93742b0f0152970e4ebe2cb8635f3d54468fb8

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+    if array.length <= 1
+        return array.join
+    elsif array.length == 2
+        return array.join(" and ")
+    else
+        str_arr = array.map.with_index {|word, index|
+            if index < array.length - 2
+                word + ", "
+            elsif index == array.length - 2
+                word + ", and "
+            else
+                word
+            end
+        }
+        return str_arr.join
+    end
 end
